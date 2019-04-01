82a05562421ab27c038bfc6a018b1b7d7f361d9e
Karim Ismail <karimismail3@cmail.carleton.ca>
2019-04-01 14:59:09 -0400

    Added a working test file

---
+++ Arduino/Controller board/First_Attemppt_Rx_Apriol1sr.ino
@@ -0,0 +1,24 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+
+int command;
+RH_ASK rf_driver;
+//
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin( 9600 );
+  rf_driver.init();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ uint8_t buf[RH_ASK_MAX_MESSAGE_LEN] = {0};
+  uint8_t buflen = sizeof(buf);
+  if (rf_driver.recv(buf, &buflen)){
+    //command = figureOutWhatItIs(buff); 
+     Serial.println((char*)buf);
+  }
+
+}
